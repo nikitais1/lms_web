db9bed4b16bea4ddda4de09e0b1bc0ed9f372d25
Nikita <nikita.isaev2001@mail.ru>
2024-04-02 23:19:31 +0300

    25.2 дополнение 2.0

--- course/serializers.py
+++ course/serializers.py
@@ -7,10 +7,17 @@ from lesson.models import Lesson
 class CourseSerializer(serializers.ModelSerializer):
     num_lessons = serializers.SerializerMethodField()
     lesson_list = serializers.SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_is_subscribed(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            return CourseSubscribe.objects.filter(user=user, course=obj).exists()
+        return False
 
     def get_num_lessons(self, course):
         return Lesson.objects.filter(course=course).count()
